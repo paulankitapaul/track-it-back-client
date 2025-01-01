21803035b066e4364046a3ad9786f892e65ff59e
paulankitapaul <paulankita.ctg@gmail.com>
2025-01-01 20:12:27 +0600

    footer added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import { Helmet } from 'react-helmet-async';
+import Footer from '../components/Footer';
 
 const MainLayout = () => {
     return (
@@ -11,6 +12,7 @@ const MainLayout = () => {
             </Helmet>
             <NavBar></NavBar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
